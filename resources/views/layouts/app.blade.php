c14e6fd6a891e221667bdb0f57fd5f36be87ffe6
deeem <karpov.dimon@gmail.com>
2017-12-16 21:20:55 +0200

    platform filter dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,18 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         &nbsp;
+
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Платформы <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                          @foreach(App\Platform::all() as $platform)
+                            <li><a href="/games/{{ $platform->slug }}">{{ $platform->name }}</a></li>
+                          @endforeach
+                        </ul>
+                      </li>
+
                     </ul>
+
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
