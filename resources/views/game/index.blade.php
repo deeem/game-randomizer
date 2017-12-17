748870fdff6eebf2a69472304179afe88d7f1db0
deeem <karpov.dimon@gmail.com>
2017-12-17 20:05:29 +0200

    game list has title

--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
         <div class="panel panel-default">
-            <div class="panel-heading"><h3 class="panel-title">Games</h3></div>
+            <div class="panel-heading"><h3 class="panel-title">Игры на платформе <strong>{{ $games->first()->name }}</strong></h3></div>
 
             <table class="table">
                 <tr>
