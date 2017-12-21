7a90e31439ca5b42003a1b50b4b1221331fb6978
deeem <karpov.dimon@gmail.com>
2017-12-21 20:03:27 +0200

    initial stats

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Game;
+use App\Platform;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,8 +25,24 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $countGames = function() {
+            $counts = [];
+            $platforms = Platform::all();
+            foreach($platforms as $platform) {
+                $counts[$platform->name] = $platform->games->count();
+            }
+
+            return $counts;
+        };
+
+        $counts = $countGames();
+
+        $max = array_reduce(array_values($counts), function ($acc, $item) {
+            return $item > $acc ? $item : $acc;
+        }, 0);
+
         $games = Game::where('user_id', '!=', null)->latest('updated_at')->take(10)->get();
 
-        return view('dashboard.index', compact('games'));
+        return view('dashboard.index', compact('games', 'counts', 'max'));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,6 +4,21 @@
 <div class="row">
   <div class="col-md-8 col-md-offset-2">
 
+    @if(count($counts))
+    <div class="panel panel-default">
+      <div class="panel-heading"></h3 class="panel-title">Добавлено игр</h3></div>
+      <div class="panel-body">
+      @foreach($counts as $name => $value)
+        <div class="progress">
+          <div class="progress-bar" style="width: {{ $value / $max * 100 }}%;">
+            {{ $name }}
+          </div>
+        </div>
+      @endforeach
+      </div>
+    </div>
+    @endif
+
     @if(count($games))
 
     <div class="panel panel-default">
