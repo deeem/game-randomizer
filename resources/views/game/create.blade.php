aca0963fd2df7b2b27e520e1e325df6e25c37e4e
deeem <karpov.dimon@gmail.com>
2018-01-19 20:32:35 +0200

    add email field

--- resources/views/game/create.blade.php
+++ resources/views/game/create.blade.php
@@ -21,15 +21,15 @@
           @endif
 
           <div class="form-group">
-            <label for="inputName" class="col-sm-4 control-label">Название</label>
-            <div class="col-sm-8">
-              <input type="text" class="form-control" id="inputName" placeholder="Имя" name="name" required>
+            <label for="inputName" class="col-sm-3 control-label">Название</label>
+            <div class="col-sm-6">
+              <input type="text" class="form-control" id="inputName" placeholder="Название игры" name="name" required>
             </div>
           </div>
 
           <div class="form-group">
-            <label for="platform_id" class="col-sm-4 control-label">Платформа</label>
-            <div class="col-sm-8">
+            <label for="platform_id" class="col-sm-3 control-label">Платформа</label>
+            <div class="col-sm-6">
                 @foreach($platforms as $platform)
                   <div class="radio">
                     <label>
@@ -43,15 +43,22 @@
 
           @guest
           <div class="form-group">
-            <label for="inputSuggest" class="col-sm-4 control-label">Предложил</label>
-            <div class="col-sm-8">
-              <input type="text" class="form-control" id="inputSuggest" placeholder="Ваш ник" name="suggested" required>
+            <label for="inputSuggest" class="col-sm-3 control-label">Предложил</label>
+            <div class="col-sm-6">
+              <input type="text" class="form-control" id="inputSuggest" placeholder="Ваше имя (не обязательно)" name="suggester_name">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="inputSuggest" class="col-sm-3 control-label"></label>
+            <div class="col-sm-6">
+              <input type="text" class="form-control" id="inputSuggest" placeholder="Ваш email (не обязательно)" name="suggester_email">
             </div>
           </div>
           @endguest
 
           <div class="form-group">
-            <div class="col-sm-offset-4 col-sm-8">
+            <div class="col-sm-offset-4 col-sm-6">
               <button type="submit" class="btn btn-primary">Добавить</button>
             </div>
           </div>
@@ -61,6 +68,9 @@
           <p class="text-primary">
             Ознакомтесь с причинами, по которым предложенная игра может быть
             <a href="{{ route('rules.index') }}" class="alert-link"> <strong>отклонена</strong></a>
+          </p>
+          <p class="text-primary">
+            Укажите email, если хотите получить уведомление о том добавлена Ваша игра или отклонена (и почему)
           </p>
 
         </form>
