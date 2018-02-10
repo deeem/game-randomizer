c2085d3d6a64b959f65395efb0dd99b95ecdd523
deeem <karpov.dimon@gmail.com>
2018-02-10 12:32:04 +0200

    refactor randomizer

--- resources/views/randomizer/result.blade.php
+++ resources/views/randomizer/result.blade.php
@@ -6,34 +6,35 @@
   platform_id = {{ $platform->id }};
 </script>
 
-<div class="row">
+@push('scripts')
+<script src="https://unpkg.com/vue@2.5.7/dist/vue.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script src="{{ asset('js/randomizer.js') }}"></script>
+@endpush
+
+<div id="randomizer">
+
+  <div class="row">
     <div class="col-md-6 col-md-offset-3">
 
-      <div id="randomizer">
-
-        <div class="randomizer-items">
-          <div
-            v-for="game in games"
-            v-bind:class="game.class"
-            class="randomizer-item">
-            @{{ game.name }}
-          </div>
+      <div class="randomizer-items">
+        <div class="randomizer-item" v-for="game in games" v-bind:class="game.class">
+          @{{ game.name }}
         </div>
-
-        <button class="btn btn-primary"
-          v-on:click="roll"
-          v-bind:disabled="availableGames.length === 1 ? true : false">
-          roll
-        </button>
-
       </div>
 
-      @push('scripts')
-        <script src="https://unpkg.com/vue@2.5.7/dist/vue.js"></script>
-        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-        <script src="{{ asset('js/randomizer.js') }}"></script>
-      @endpush
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="text-center">
+
+      <button class="btn btn-primary" v-on:click="roll" v-bind:disabled="availableGames.length === 1 ? true : false">
+        roll
+      </button>
 
     </div>
+  </div>
+
 </div>
 @endsection
