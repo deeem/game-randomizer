176c9b2ad76b9c1b94cbc3e5629b1a92066f5820
deeem <karpov.dimon@gmail.com>
2018-02-15 16:58:13 +0200

    create buttons in empty list

--- resources/views/platform/empty.blade.php
+++ resources/views/platform/empty.blade.php
@@ -1,6 +1,16 @@
 <div class="panel panel-default">
-  <div class="panel-heading"><h3 class="panel-title">Платформы</h3></div>
+
+  <div class="panel-heading clearfix">
+    <h3 class="panel-title pull-left">Платформы</h3>
+    <div class="btn-group pull-right">
+      <a href="{{ route('platforms.create') }}" class="btn btn-primary btn-sm">
+        <i class="fa fa-plus" aria-hidden="true"></i>
+      </a>
+    </div>
+  </div>
+
   <div class="panel-body">
     <p class="text-center">Список пуст</p>
   </div>
+
 </div>
