a137a642e87cdf8eb7db206b75d71f9d49147892
deeem <karpov.dimon@gmail.com>
2018-03-30 18:44:44 +0300

    randomizer-platform-list animation

--- resources/views/randomizer/list.blade.php
+++ resources/views/randomizer/list.blade.php
@@ -1,7 +1,7 @@
-<div class="randomizer-list">
+<div class="randomizer-platform-list">
   @foreach($platforms as $platform)
 
-  <a href="{{ route('randomizer', ['platform' => $platform->slug]) }}">
+  <a href="{{ route('randomizer', ['platform' => $platform->slug]) }}" class="randomizer-platform-list-item">
       {{ $platform->name }}
   </a>
 
